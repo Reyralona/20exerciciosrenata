849803cb40c30569bb95ecdc4a3007da1414f7f9
Reyralona <91705984+Reyralona@users.noreply.github.com>
2023-04-30 01:39:24 +0000

    Último exercício desbugado

--- renata20/exec20/exec20.c
+++ renata20/exec20/exec20.c
@@ -5,59 +5,39 @@
 
 struct aluno {
     char nome[20];
-    int notas[2];
-    int media;
+    int  notas[2];
+    int  media;
 };
-
-void swap(int* xp, int* yp){
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n - 1; i++){
-        // Last i elements are already in place
-        for (j = 0; j < n - i - 1; j++){
-            if (arr[j] > arr[j + 1]){
-                swap(&arr[j], &arr[j + 1]);
-            }
-        }
-    }
-}
 
 int main () {
 
-    struct aluno alunos[5];
-    int medias[5];
+    struct aluno alunos[5], temp;
 
     //registra alunos e notas
     for (int i = 0; i < 5; i ++){
         printf("Insira o nome do aluno numero %d >> \n" , i + 1);
-        scanf("%s", &alunos[i].nome);
+        scanf("%s", alunos[i].nome);
+
         printf("Insira as notas de %s >> \n" , alunos[i].nome);
         scanf("%d%d", &alunos[i].notas[0], &alunos[i].notas[1]);
+        
+        alunos[i].media = alunos[i].notas[0] + alunos[i].notas[1];
         printf("\n");
     }
 
-    //calcula as médias e armazena num vetor
-    for (int i = 0; i < 5; i ++){
-        alunos[i].media = (alunos[i].notas[0] + alunos[i].notas[1]);
-        medias[i] = alunos[i].media;
-    }
-
-    //ordena o vetor
-    bubbleSort(medias, 5);
-
-    //retorna os alunos ordenados pelas médias
-    for (int i = 0; i < 5; i ++){
+    for (int i = 0; i < 5; i++){
         for (int j = 0; j < 5; j++){
-            if (alunos[j].media == medias[i]){
-                printf("O aluno %s tem media >> %d\n", alunos[j].nome, medias[i]);
+            if (alunos[i].media < alunos[j].media){
+                temp = alunos[i];
+                alunos[i] = alunos[j];
+                alunos[j] = temp;
             }
         }
     }
-    
+
+    for (int i = 0; i < 5; i++){
+        printf("%s tirou %d\n", alunos[i].nome, alunos[i].media);
+    }
+
     return 0;
 }
